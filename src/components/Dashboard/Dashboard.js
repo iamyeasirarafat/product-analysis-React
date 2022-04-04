0009ff79ece685d664b4d5e298ba6e9aede32dff
iamyeasirarafat <yas.yeasir@gmail.com>
2022-04-04 15:10:24 +0600

    active link done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {
-    Bar,
-    BarChart,
+  Bar,
+  BarChart,
   CartesianGrid,
   Legend,
   Line,
@@ -19,38 +19,43 @@ const Dashboard = () => {
       .then((response) => response.json())
       .then((data) => setData(data));
   }, []);
-  return ( 
-      <div>
-          <Nav></Nav>
-          <h2 className="text-center text-4xl uppercase font-semibold text-blue-600 mt-14">Our Monthly Sells</h2>
-    <div className="md:flex pt-14 w-3/4 mx-auto" >
-      <div className="">
-        <h2 className="text-center text-blue-500 font-semibold mb-3 text-xl">Month wise sell</h2>
-        <LineChart data={data} width={400} height={300}>
-          <Line stroke="#8884d8" type="monotone" dataKey="sell"></Line>
-          <XAxis dataKey="month" />
-          <YAxis></YAxis>
-          <Legend></Legend>
-          
-          <CartesianGrid strokeDasharray = "3 3"></CartesianGrid>
-          <Tooltip></Tooltip>
-        </LineChart>
-      </div>
-      <div className="ml-auto">
-        <h2 className="text-center text-blue-500 font-semibold mb-3 text-xl">invesment vs Revenue</h2>
-        <BarChart width ={400} height ={300} data={data}>
-        <CartesianGrid strokeDasharray = "3 3"></CartesianGrid>
-        <XAxis dataKey ="month"></XAxis>
-        <YAxis></YAxis>
-        <Tooltip></Tooltip>
-        <Legend></Legend>
-        <Bar dataKey="revenue" stackId="a" fill="#8884d8" ></Bar>
-        <Bar dataKey="investment" stackId="a" fill="#82ca9d"></Bar>
-        </BarChart>
-      </div>
-    </div>
-    </div>
+  return (
+    <div>
+      <Nav></Nav>
+      <h2 className="text-center text-4xl uppercase font-semibold text-blue-600 mt-14">
+        Our Monthly Sells
+      </h2>
+      <div className="md:flex pt-14 w-3/4 mx-auto">
+        <div className="">
+          <h2 className="text-center text-blue-500 font-semibold mb-3 text-xl">
+            Month wise sell
+          </h2>
+          <LineChart data={data} width={400} height={300}>
+            <Line stroke="#8884d8" type="monotone" dataKey="sell"></Line>
+            <XAxis dataKey="month" />
+            <YAxis></YAxis>
+            <Legend></Legend>
 
+            <CartesianGrid strokeDasharray="3 3"></CartesianGrid>
+            <Tooltip></Tooltip>
+          </LineChart>
+        </div>
+        <div className="ml-auto">
+          <h2 className="text-center text-blue-500 font-semibold mb-3 text-xl">
+            invesment vs Revenue
+          </h2>
+          <BarChart width={400} height={300} data={data}>
+            <CartesianGrid strokeDasharray="3 3"></CartesianGrid>
+            <XAxis dataKey="month"></XAxis>
+            <YAxis></YAxis>
+            <Tooltip></Tooltip>
+            <Legend></Legend>
+            <Bar dataKey="revenue" stackId="a" fill="#8884d8"></Bar>
+            <Bar dataKey="investment" stackId="a" fill="#82ca9d"></Bar>
+          </BarChart>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <nav className=" justify-center flex mt-6">
             <ul className="flex ">
-                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><Link to = "/">HOME</Link></li>
-                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><Link to = "/reviews">REVIEWS</Link></li>
-                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><Link to = "/dashboard">DASHBOARD</Link></li>
-                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><Link to = "/blogs">BLOGS</Link></li>
-                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><Link to = "/about">ABOUT</Link></li>
+                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><NavLink  style={({ isActive }) => {
+             return {color: isActive ? "rgb(244 114 182)" : "",};
+             }}  to = "/">HOME</NavLink></li>
+                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><NavLink  style={({ isActive }) => {
+             return {color: isActive ? "rgb(244 114 182)" : "",};
+             }} to = "/reviews">REVIEWS</NavLink></li>
+                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><NavLink  style={({ isActive }) => {
+             return {color: isActive ? "rgb(244 114 182)" : "",};
+             }} to = "/dashboard">DASHBOARD</NavLink></li>
+                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><NavLink style={({ isActive }) => {
+             return {color: isActive ? "rgb(244 114 182)" : "",};
+             }}  to = "/blogs">BLOGS</NavLink></li>
+                <li className="mr-2 duration-200 ease-in font-semibold px-4 pb-1 hover:text-pink-400 hover:border-pink-400 border-b-2"><NavLink style={({ isActive }) => {
+             return {color: isActive ? "rgb(244 114 182)" : "",};
+             }}  to = "/about">ABOUT</NavLink></li>
+               
             </ul>
         </nav>
     );
